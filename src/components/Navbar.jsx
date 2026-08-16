ee72faddb8f68356a963344f8a6fc694dfde5f36
Vinisha Gupta <vinishasgupta@gmail.com>
2026-08-17 01:47:56 +0530

    added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {navLinks}from '../constants'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <header>
+        <nav>
+            <img src='/logo.svg' alt='logo'></img>
+            <ul>
+              {navLinks.map(({label})=>
+              <li key={label}>
+                <a href={label}>{label}</a>
+              </li>
+              )}
+            </ul>
+            <div className='flex-center gap-3'>
+              <button>
+                <img src='./search.svg' alt='search'/>
+              </button>
+              <button>
+                <img src='./cart.svg' alt='cart'/>
+              </button>
+            </div>
+        </nav>
+    </header>
   )
 }
 
